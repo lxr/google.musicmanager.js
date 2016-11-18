84cc5251c6fbe554120ffd69a986b8a8afff0f8d
Lari Rasku <lari.o.rasku@gmail.com>
2016-11-19 00:29:12 +0200

    Bump LICENSE year

--- google.musicmanager.js
+++ google.musicmanager.js
@@ -1,6 +1,6 @@
 /*!
 
-Copyright (c) 2014-5 Lari Rasku <lari.o.rasku@gmail.com>
+Copyright (c) 2014-6 Lari Rasku <lari.o.rasku@gmail.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a
 copy of this software and associated documentation files (the
